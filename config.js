3f8f62fa653b643c5fc934cd1274282c5709b006
senu22x <senudisanjana58@gmail.com>
2024-11-15 09:51:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "mongodb://mongo:AYvHrarNvanEDCmMHpNRJHccHliJvhwn@autorack.proxy.rlwy.net:56460",
+MONGODB: process.env.MONGODB || "mongodb://mongo:mZCJsPBVTkIczOQFdIZgLpFIVvsFgyeH@autorack.proxy.rlwy.net:13889",
 FOOTER: process.env.FOOTER || "Mr Dila",
 BTN: process.env.BTN || "Click Here",
 BTNURL: process.env.BTNURL || "https://whatsapp.com/channel/0029VarK8LEHbFVFcPRvxl3T",
